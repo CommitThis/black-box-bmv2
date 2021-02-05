265d546ff37b211d335609bc59f30d2c4fe0417b
G Davey <gdavey@committhis.co.uk>
2021-02-05 13:48:49 +0000

    Improved error handling in compile so user can see errors

--- bmv2-docker/simple_switch/compile.py
+++ bmv2-docker/simple_switch/compile.py
@@ -15,20 +15,36 @@ def compile_p4(source_dir, main):
     p4info = os.path.join(out_dir, f'{name}.p4info.txt')
 
     if os.path.exists(tmp_dir):
+        print(f'deleting {tmp_dir}')
         rmtree(tmp_dir)
-
+    
     copytree(source_dir, tmp_dir)
     compile_command = f'p4c {main} -o {out_dir} -I{tmp_dir} --p4runtime-files={p4info}'.split()
     print(compile_command)
     client = DockerClient(base_url="unix://var/run/docker.sock", version='auto')
-    x = client.containers.run(
+    x = None
+
+    container = client.containers.run(
         SimpleSwitchDocker.CONTAINER,
         compile_command,
-        auto_remove=True,
         working_dir=tmp_dir,
-        volumes={tmp_dir: {'bind': tmp_dir, 'mode': 'rw'}}
+        volumes={tmp_dir: {'bind': tmp_dir, 'mode': 'rw'}},
+        detach=True
     )
-    
+
+    status = container.wait()
+
+    logs = []
+    print()
+    for line in container.logs(stream=True):
+        line = line.decode('utf-8').replace('\n', '')
+        print(line)
+    print()
+    container.remove()
+
+    if status != 0:
+        raise Exception('Error running compile')
+
     result_dir = os.path.join(source_dir, 'out')
     compiled = os.path.join(result_dir, f'{name}.json')
 
