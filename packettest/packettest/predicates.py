dcb554397b80f10c4e78167c2725e3dc10e67ae1
G Davey <gdavey@committhis.co.uk>
2021-02-11 09:41:12 +0000

    Rewrote saw packet predicates Added PCAP writer wrapper Convenience predicate for counting a specific packet.

--- packettest/packettest/predicates.py
+++ packettest/packettest/predicates.py
@@ -3,179 +3,213 @@ import enum
 
 
 class TimedOut:
-	pass
+    pass
+
+
+class Write:
+    def __init__(self, file, predicate):
+        self._file = file
+        self._pred = predicate
+
+    def on_packet(self, pkt):
+        self._pred.on_packet(pkt)
+
+    def stop_condition(self, pkt) -> bool:
+        self._file.write(pkt)
+        return self._pred.stop_condition(pkt)
+
+    def on_finish(self, timed_out):
+        self._pred.on_finish(timed_out)
+
 
 
 
 class Predicate:
-	def on_packet(self, pkt):
-		pass
+    def on_packet(self, pkt):
+        pass
 
-	def stop_condition(self, pkt) -> bool:
-		pass
+    def stop_condition(self, pkt) -> bool:
+        pass
 
-	def on_finish(self, timed_out):
-		pass
+    def on_finish(self, timed_out):
+        pass
+
+
 
 '''
 It is quicker to find something than not find something!
 '''
-
 class received_packet(Predicate):
-	''' Will only get called if a packet is received '''
-	def stop_condition(self, pkt):
-		return True
+    ''' Will only get called if a packet is received '''
+    def stop_condition(self, pkt):
+        return True
 
-	def on_finish(self, timed_out):
-		return not timed_out
+    def on_finish(self, timed_out):
+        return not timed_out
 
 
 
 class timed_out(Predicate):
-	def stop_condition(self, pkt):
-		return True
+    def stop_condition(self, pkt):
+        return True
 
-	def on_finish(self, timed_out):
-		return timed_out
+    def on_finish(self, timed_out):
+        return timed_out
 
 
 
 class saw_src_mac(Predicate):
-	def __init__(self, mac):
-		self._mac = mac
+    def __init__(self, mac):
+        self._mac = mac
 
-	''' Need to do result setting and stop condition here as the stop filter is
-		called before the packet function '''
-	def stop_condition(self, pkt):
-		return pkt.haslayer(Ether) and pkt[Ether].src == self._mac
+    ''' Need to do result setting and stop condition here as the stop filter is
+        called before the packet function '''
+    def stop_condition(self, pkt):
+        return pkt.haslayer(Ether) and pkt[Ether].src == self._mac
 
-	def on_finish(self, timed_out):
-		return not timed_out
+    def on_finish(self, timed_out):
+        return not timed_out
 
 
 
 class did_not_see_src_mac(Predicate):
-	def __init__(self, mac):
-		super().__init__()
-		self._mac = mac
-		self._results = []
+    def __init__(self, mac):
+        super().__init__()
+        self._mac = mac
+        self._results = []
 
-	def on_packet(self, pkt):
-		if pkt.haslayer(Ether):
-			self._results.append(pkt[Ether].src)
+    def on_packet(self, pkt):
+        if pkt.haslayer(Ether):
+            self._results.append(pkt[Ether].src)
 
-	def on_finish(self, timed_out):
-		return self._mac not in self._results
+    def on_finish(self, timed_out):
+        return self._mac not in self._results
 
 
 
 class saw_dst_mac(Predicate):
-	def __init__(self, mac):
-		self._mac = mac
+    def __init__(self, mac):
+        self._mac = mac
 
-	''' Need to do result setting and stop condition here as the stop filter is
-		called before the packet function '''
-	def stop_condition(self, pkt):
-		return pkt.haslayer(Ether) and pkt[Ether].dst == self._mac
+    ''' Need to do result setting and stop condition here as the stop filter is
+        called before the packet function '''
+    def stop_condition(self, pkt):
+        return pkt.haslayer(Ether) and pkt[Ether].dst == self._mac
 
-	def on_finish(self, timed_out):
-		return not timed_out
+    def on_finish(self, timed_out):
+        return not timed_out
 
 
 
 class did_not_see_dst_mac(Predicate):
-	def __init__(self, mac):
-		super().__init__()
-		self._mac = mac
-		self._results = []
+    def __init__(self, mac):
+        super().__init__()
+        self._mac = mac
+        self._results = []
 
-	def on_packet(self, pkt):
-		if pkt.haslayer(Ether):
-			self._results.append(pkt[Ether].dst)
+    def on_packet(self, pkt):
+        if pkt.haslayer(Ether):
+            self._results.append(pkt[Ether].dst)
 
-	def on_finish(self, timed_out):
-		return self._mac not in self._results
+    def on_finish(self, timed_out):
+        return self._mac not in self._results
 
 
 
 class saw_vlan_tag(Predicate):
-	def __init__(self, tag):
-		self._tag = tag
+    def __init__(self, tag):
+        self._tag = tag
 
-	def stop_condition(self, pkt):
-		return pkt.haslayer(Dot1Q) and pkt[Dot1Q].vlan == self._tag
+    def stop_condition(self, pkt):
+        return pkt.haslayer(Dot1Q) and pkt[Dot1Q].vlan == self._tag
 
-	def on_finish(self, timed_out):
-		return not timed_out
+    def on_finish(self, timed_out):
+        return not timed_out
 
 
 
 class did_not_see_vlan(Predicate):
+    def stop_condition(self, pkt):
+        return pkt.haslayer(Dot1Q)
 
-	def stop_condition(self, pkt):
-		return pkt.haslayer(Dot1Q)
-
-	def on_finish(self, timed_out):
-		return timed_out
+    def on_finish(self, timed_out):
+        return timed_out
 
 
 
 class saw_packet_equals_sent(Predicate):
-	def __init__(self, pkt):
-		super().__init__()
+    def __init__(self, packet):
         # Reload packet to generate CRC
-		tmp = pkt.__class__(bytes(pkt))
+        self._packet = packet.__class__(bytes(packet))
+        self._saw_pkt = False
 
-		self._pkt = tmp
+    def stop_condition(self, packet):
+        self._saw_pkt = packet == self._packet
+        return self._saw_pkt
 
-	def stop_condition(self, pkt):
-		return self._pkt == pkt
+    def on_finish(self, timed_out):
+        return self._saw_pkt
 
-	def on_finish(self, timed_out):
-		return not timed_out
 
 
 class did_not_see_packet_equals_sent(Predicate):
-	def __init__(self, pkt):
-		super().__init__()
-		tmp = pkt.__class__(bytes(pkt))
-		self._pkt = tmp
-		self._saw_pkt = False
+    def __init__(self, packet):
+        self._packet = packet.__class__(bytes(packet))
+        self._saw_pkt = False
 
-	# def stop_condition(self, pkt):
-	# 	return self._pkt == pkt
-
-	def on_packet(self, pkt):
-		if pkt == self._pkt:
-			self._saw_pkt = True
-
-	def on_finish(self, timed_out):
-		return not self._saw_pkt
-
+    def stop_condition(self, packet):
+        self._saw_pkt = packet == self._packet
+        return self._saw_pkt
+        
+    def on_finish(self, timed_out):
+        return not self._saw_pkt
 
 
 
 class packet_count_was(Predicate):
+    def __init__(self, count):
+        self._expected_count = count
+        self._received_count = 0
 
-	def __init__(self, count):
-		self._expected_count = count
-		self._received_count = 0
+    def on_packet(self, pkt):
+        self._received_count += 1
 
-	def on_packet(self, pkt):
-		self._received_count += 1
+    def on_finish(self, timed_out):
+        return self._received_count == self._expected_count
 
-	def on_finish(self, timed_out):
-		return self._received_count == self._expected_count
-		
 
 
 class packet_count(Predicate):
+    def __init__(self):
+        self._received_count = 0
 
-	def __init__(self):
-		self._received_count = 0
+    def on_packet(self, pkt):
+        self._received_count += 1
 
-	def on_packet(self, pkt):
-		self._received_count += 1
+    def on_finish(self, timed_out):
+        return self._received_count
 
-	def on_finish(self, timed_out):
-		return self._received_count
+
+class only_saw(Predicate):
+    def __init__(self, packet):
+        self._packet = packet.__class__(bytes(packet))
+        self._seen_count = None
+        self._expected = None
+        self._method = None
+
+    def on_packet(self, packet):
+        if self._packet == packet:
+            self._seen_count += 1
+    
+    def on_finish(self):
+        return self._seen_count == self._expected
+
+    def once(self):
+        self._count = 1
+        return self
+
+    def count(self, n):
+        self._count = n
+        return self
+
+
